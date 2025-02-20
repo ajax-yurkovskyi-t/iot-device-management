ea5267ce76b78620f1deaba8b1d756404e993359
Taras Yurkovskyi <tarasyurkovskti@Tarass-MacBook-Pro.local>
2025-02-20 10:17:55 +0200

    test

--- iot-management-device/user/src/main/kotlin/com/example/iotmanagementdevice/user/infrastructure/rest/controller/UserController.kt
+++ iot-management-device/user/src/main/kotlin/com/example/iotmanagementdevice/user/infrastructure/rest/controller/UserController.kt
@@ -37,6 +37,11 @@ class UserController(
             .map { userDtoMapper.toDto(it) }
     }
 
+    @GetMapping("/test")
+    fun getUserByString(): Mono<String> {
+        return Mono.just("user")
+    }
+
     @PutMapping("{id}")
     fun update(
         @PathVariable id: String,
