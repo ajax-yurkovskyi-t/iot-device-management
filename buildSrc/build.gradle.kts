e5327946b905a4937d23e9d89fba7a973b7950c8
Taras Yurkovskyi <tarasyurkovskti@Tarass-MacBook-Pro.local>
2025-02-20 10:04:10 +0200

    test

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
-    implementation("io.github.surpsg:delta-coverage-gradle:2.5.0")
+    implementation("io.github.gw-kit.delta-coverage:io.github.gw-kit.delta-coverage.gradle.plugin:3.2.0")
     implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.6")
     implementation("org.springframework.boot:spring-boot-gradle-plugin:3.3.2")
     implementation("org.jetbrains.kotlin:kotlin-allopen:1.9.23")
